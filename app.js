a2adde5e34f837a8461006499d8c0c8bae67f77c
ejyang <trustnloveu@gmail.com>
2021-06-30 16:22:47 +0900

    Create custom route module, Seperate code from app.js

--- app.js
+++ app.js
@@ -6,62 +6,11 @@
 // path
 // os
 
+//* Module
 const http = require("http");
-const fs = require("fs");
+const routes = require("./routes");
 
-const server = http.createServer((req, res) => {
-  // console.log(req);
-  // console.log(req.url);       /text
-  // console.log(req.method);    GET
-  // console.log(req.headers);   { request header info }
-
-  const url = req.url;
-  const method = req.method;
-
-  if (url === "/") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>My First Page</title></head>");
-    res.write("<body>");
-    res.write(
-      '<form action="/message" method="POST"><input type="text" name="mesage"><button type="submit">Move</button></button></form>'
-    );
-    res.write("</body>");
-    res.write("</html>");
-    return res.end();
-  }
-
-  if (url === "/message" && method === "POST") {
-    const body = [];
-
-    //* data > chunk = <Buffer 6d 65 73 61 67 65 3d 74 65 73 74>
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-
-    //* end > parsed result > mesage = test
-    return req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-
-      //* create file (Sync || Async)
-      // fs.writeFileSync("message.txt", message);
-      fs.writeFile("message.txt", message, (err) => {
-        res.statusCode = 302; // res.writeHead(302);     > redirection
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  }
-
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My First Page</title></head>");
-  res.write("<body><h1>Hello from My Node.js Server.</h1></body>");
-  res.write("</html>");
-  res.end();
-
-  // process.exit();
-});
+//* Server
+const server = http.createServer(routes);
 
 server.listen(3000);
---
+++ routes.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+  // console.log(req);
+  // console.log(req.url);       /text
+  // console.log(req.method);    GET
+  // console.log(req.headers);   { request header info }
+
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body>");
+    res.write(
+      '<form action="/message" method="POST"><input type="text" name="mesage"><button type="submit">Move</button></button></form>'
+    );
+    res.write("</body>");
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+
+    //* data > chunk = <Buffer 6d 65 73 61 67 65 3d 74 65 73 74>
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    //* end > parsed result > mesage = test
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+
+      //* create file (Sync || Async)
+      // fs.writeFileSync("message.txt", message);
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302; // res.writeHead(302);     > redirection
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from My Node.js Server.</h1></body>");
+  res.write("</html>");
+  res.end();
+
+  // process.exit();
+};
+
+module.exports = requestHandler;

358b5049fbe138659a9c1143bf94acfda39c7a8a
ejyang <trustnloveu@gmail.com>
2021-08-26 23:04:24 +0900

    Set a simple user page with PUG

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-app.set("view engine", "ejs");
+const users = [];
+
+app.set("view engine", "pug");
 app.set("views", "views");
 
 // Routes ...
@@ -14,7 +16,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // Navigations
-// ...
+// index
+app.get("/", (req, res, next) => {
+  res.render("index", {
+    pageTitle: "Add User",
+  });
+});
+
+// users
+app.get("/users", (req, res, next) => {
+  res.render("users", {
+    pageTitle: "Users",
+    users: users,
+  });
+});
+
+// add-user
+app.post("/add-user", (req, res, next) => {
+  users.push({ name: req.body.username });
+  res.redirect("/users");
+});
 
 // 404 Page
 app.use((req, res, next) => {

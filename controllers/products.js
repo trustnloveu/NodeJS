11a7e521ac6d4aa1faffa1dce970fff870708e37
ejyang <trustnloveu@gmail.com>
2021-08-31 13:10:54 +0900

    Set basic structure to extend the app

--- controllers/products.js
+++ controllers/products.js
@@ -3,7 +3,7 @@ const Product = require("../models/product");
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
-    res.render("shop", {
+    res.render("shop/product-list", {
       prods: products,
       pageTitle: "Shop",
       path: "/",
@@ -12,7 +12,7 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("add-product", {
+  res.render("admin/add-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
   });

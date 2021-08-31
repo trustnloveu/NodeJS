f27a708ee71949ae4db92adedb714843d2c0e9c9
ejyang <trustnloveu@gmail.com>
2021-08-31 12:48:26 +0900

    Define Product class, declare save and fetach methods to store and render data which will be saved into .json file

--- controllers/products.js
+++ controllers/products.js
@@ -2,12 +2,12 @@
 const Product = require("../models/product");
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll();
-
-  res.render("shop", {
-    prods: products,
-    pageTitle: "Shop",
-    path: "/",
+  Product.fetchAll((products) => {
+    res.render("shop", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+    });
   });
 };
 
--- models/product.js
+++ models/product.js
@@ -1,15 +1,52 @@
+const fs = require("fs");
+const path = require("path");
+
 const products = [];
 
 module.exports = class Product {
+  // constructor
   constructor(title) {
     this.title = title;
   }
 
+  // save
   save() {
-    products.push(this);
+    const filePath = path.join(
+      path.dirname(require.main.filename),
+      "data",
+      "products.json"
+    );
+
+    fs.readFile(filePath, (error, fileContent) => {
+      let products = [];
+
+      if (!error) {
+        products = JSON.parse(fileContent);
+      }
+
+      products.push(this);
+
+      fs.writeFile(filePath, JSON.stringify(products), (error) => {
+        console.log(error);
+      });
+    });
   }
 
-  static fetchAll() {
+  // fetchAll
+  static fetchAll(callBack) {
+    const filePath = path.join(
+      path.dirname(require.main.filename),
+      "data",
+      "products.json"
+    );
+
+    fs.readFile(filePath, (error, fileContent) => {
+      if (error) {
+        callBack([]);
+      }
+      callBack(JSON.parse(fileContent));
+    });
+
     return products;
   }
 };

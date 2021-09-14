253a1e23b569ae23f9ac8d1efb2e054065f70705
ejyang <trustnloveu@gmail.com>
2021-09-14 23:31:14 +0900

    Fix errors in Cart and Product Model

--- controllers/admin.js
+++ controllers/admin.js
@@ -15,7 +15,7 @@ exports.postAddProduct = (req, res, next) => {
   const description = req.body.description;
   const price = req.body.price;
 
-  const product = new Product(null, title, imageUrl, description, price);
+  const product = new Product(null, title, price, imageUrl, description);
 
   product.save();
 

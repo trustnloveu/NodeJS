b1d550a38c5c8b6ec03a07463c9952079f6d3efe
ejyang <trustnloveu@gmail.com>
2021-10-03 14:21:40 +0900

    Set Cookie through request header

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,16 @@
 exports.getLogin = (req, res, next) => {
+  const isLogin = req.get("Cookie").split(";")[0].trim().split("=")[1]; // split(;) > index > find > split(=) > value
+
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
+    isAuthenticated: isLogin,
   });
+};
+
+exports.postLogin = (req, res, next) => {
+  //! Cookie
+  res.setHeader("Set-Cookie", "login=true");
+
+  res.redirect("/");
 };
--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 const authController = require("../controllers/auth");
 
-//* get > /login
+//* GET > /login
 router.get("/login", authController.getLogin);
+
+//* POST > /login
+router.post("/login", authController.postLogin);
 
 module.exports = router;

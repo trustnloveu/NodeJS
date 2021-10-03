f32203aa05d1c896b993e34bd0317ef6de4d427d
ejyang <trustnloveu@gmail.com>
2021-10-03 13:46:47 +0900

    Auth router, controller and views

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ const errorController = require("./controllers/error");
 //* Routes
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 //* Utils
 app.use(express.urlencoded({ extended: false }));
@@ -42,6 +43,7 @@ app.use((req, res, next) => {
 //* Navigations
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 //* 404 : Page not found
 app.use(errorController.get404);

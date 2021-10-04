8dab1b5ff9495170ee398aeb9bf4743f808966aa
ejyang <trustnloveu@gmail.com>
2021-10-04 14:17:12 +0900

    Hash passoword with bcryptjs, Login & Signup process

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,6 @@
+//* External Libraries
+const bcrypt = require("bcryptjs");
+
 //* Models
 const User = require("../models/user");
 
@@ -6,6 +9,51 @@ exports.getLogin = (req, res, next) => {
     path: "/login",
     pageTitle: "Login",
     isAuthenticated: false,
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  //! Cookie
+  // res.setHeader("Set-Cookie", "login=true"); //! ; Max-age=10 ; Expires=Date ; Domain= ; Secure ...
+
+  //! Session
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email: email })
+    .then((user) => {
+      // Eamil Correction
+      if (!user) return res.redirect("/login");
+
+      // Password Correction (by comparing hashed password)
+      bcrypt
+        .compare(password, user.password)
+        .then((isMatch) => {
+          if (!isMatch) return res.redirect("/login");
+
+          req.session.isLogin = true;
+          req.session.user = user;
+
+          // (Optional) When you'd like to redirect views, and make it clear redirecting page after the session is saved
+          return req.session.save((error) => {
+            if (error) console.log("Session Save Error ::: " + error);
+            return res.redirect("/");
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((error) => {
+    console.log(error);
+
+    res.redirect("/");
   });
 };
 
@@ -25,16 +73,23 @@ exports.postSignUp = (req, res, next) => {
   // email validation
   User.findOne({ email: email })
     .then((user) => {
-      if (user) {
-        return res.redirect("/signup");
-      }
-      const newUser = new User({
-        email: email,
-        password: password,
-        // cart: { items: []}
-      });
+      if (user) return res.redirect("/signup");
 
-      return newUser.save();
+      // Hash Password
+      return bcrypt
+        .hash(password, 12)
+        .then((hashedPassword) => {
+          const newUser = new User({
+            email: email,
+            password: hashedPassword,
+            // cart: { items: []} //! Cart will automatically set as it defined in User Schema
+          });
+
+          return newUser.save();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     })
     .then((result) => {
       res.redirect("/login");
@@ -42,34 +97,4 @@ exports.postSignUp = (req, res, next) => {
     .catch((error) => {
       console.log(error);
     });
-};
-
-exports.postLogin = (req, res, next) => {
-  //! Cookie
-  // res.setHeader("Set-Cookie", "login=true"); //! ; Max-age=10 ; Expires=Date ; Domain= ; Secure ...
-
-  //! Session
-  User.findOne()
-    .then((user) => {
-      req.session.isLogin = true;
-      req.session.user = user;
-
-      // (Optional) When you'd like to redirect views, and make it clear redirecting page after the session is saved
-      req.session.save((error) => {
-        console.log(error);
-
-        res.redirect("/");
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-};
-
-exports.postLogout = (req, res, next) => {
-  req.session.destroy((error) => {
-    console.log(error);
-
-    res.redirect("/");
-  });
 };
